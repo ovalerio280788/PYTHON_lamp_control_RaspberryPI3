e53c943164b5aea64b48c24e09a08f6d0a031c8f
ovalerio <ovalerio@zenoss.com>
2018-03-04 21:44:28 -0600

    Lamp example

--- ledblink/views.py
+++ ledblink/views.py
@@ -6,7 +6,9 @@ from ledblink.forms import UserLedBlinkForm
 from ledblink.models import ControlLamp
 
 pin_number = 16
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(pin_number, GPIO.OUT)
 
 
@@ -21,17 +23,17 @@ def get_pin_state():
 
 def blinker(request):
     current_pin_state = get_pin_state()
-    print_timestamp(message="Current pin state: {0}".format(current_pin_state))
+    print_timestamp(message="Current pin {0} state: {1}".format(current_pin_state, pin_number))
 
     if request.method == 'POST':
         form = UserLedBlinkForm(request.POST or None)
         if form.is_valid():
             if int(request.POST['state']):
                 print_timestamp(message="Turning On pin #" + str(pin_number))
-                GPIO.output(pin_number, 0)
+                GPIO.output(pin_number, GPIO.LOW)
             else:
                 print_timestamp(message="Turning Off pin #" + str(pin_number))
-                GPIO.output(pin_number, 1)
+                GPIO.output(pin_number, GPIO.HIGH)
 
             if len(ControlLamp.objects.all()) > 1:
                 # delete all registers
